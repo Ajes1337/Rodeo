ed535aa72686f14cf4fe3e90451da8f938cbe896
Bjarke Elias Clauson-Kaas Ørrild <bjarkeelias@gmail.com>
2015-04-10 17:49:34 +0200

    no message

---
+++ Rodeo/Assets/Scripts/BaseMonoBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BaseMonoBehaviour), true)]
+public class LineBuilderComponentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ((BaseMonoBehaviour)target).OnInspectorGUI();
+    }
+}
+
+public class BaseMonoBehaviour : MonoBehaviour
+{
+    public virtual void OnInspectorGUI()
+    {
+    }
+}
