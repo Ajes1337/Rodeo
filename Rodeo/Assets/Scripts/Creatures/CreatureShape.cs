06e15fae5dc0f8b8e0f2ba520f1c484abc72ddf9
Bjarke Elias Clauson-Kaas Ørrild <bjarkeelias@gmail.com>
2015-04-10 18:05:11 +0200

    NY!

---
+++ Rodeo/Assets/Scripts/Creatures/CreatureShape.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshCollider))]
+public class CreatureShape : BaseMonoBehaviour
+{
+    private Mesh _mesh;
+    private float _sphereRadius = 1;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnGUI()
+    {
+        //if (GUI.Button()
+        //{
+        //}
+    }
+
+    private void Initialize()
+    {
+        if (_mesh == null)
+        {
+            _mesh = new Mesh();
+            this.GetComponent<MeshFilter>().sharedMesh = _mesh;
+        }
+    }
+}
