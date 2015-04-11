54d7e9ff2487107bed407de7ddc2b900a70215e7
Anders Nørgaard <Ajes1337@gmail.com>
2015-04-11 23:37:08 +0200

    dasdasda meeeh muuuh

--- Rodeo/Assets/Scripts/Creatures/Player.cs
+++ Rodeo/Assets/Scripts/Creatures/Player.cs
@@ -5,8 +5,7 @@ using System.Text;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     public static Player Instance;
     private Rigidbody _rigidbody;
     private float _forceZ = 1500;
@@ -16,63 +15,79 @@ public class Player : MonoBehaviour
     private float _rotationXSpeed = 1;
     private float _tiltSpeed = 30;
     private float _maxTiltZ = 60;
+    public Missile MissileFab;
+    private bool mouseIsLocked = true;
 
-    private void Start()
-    {
+    void OnGUI() {
+
+
+
+    }
+
+    private void Start() {
         this._rigidbody = this.GetComponent<Rigidbody>();
         Instance = this;
     }
 
-    private void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            mouseIsLocked = !mouseIsLocked;
+        }
+
+        if (mouseIsLocked) {
+            Screen.lockCursor = true;
+            Cursor.visible = false;
+        }
+        else {
+            Screen.lockCursor = false;
+            Cursor.visible = true;
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.RightControl)) {
+            Instantiate(MissileFab, transform.position + transform.forward * 2, Quaternion.LookRotation(transform.forward));
+        }
+    }
+
+    private void FixedUpdate() {
+        if (Input.GetKey(KeyCode.W)) {
             this._rigidbody.AddForce(this.transform.forward * Time.fixedDeltaTime * _forceZ);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.S)) {
             this._rigidbody.AddForce(this.transform.forward * -1 * Time.fixedDeltaTime * _forceZ);
         }
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.A)) {
             this._rigidbody.AddForce(this.transform.TransformDirection(Vector3.left) * Time.fixedDeltaTime * _forceX);
         }
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)) {
             this._rigidbody.AddForce(this.transform.TransformDirection(Vector3.right) * Time.fixedDeltaTime * _forceX);
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
+        if (Input.GetKey(KeyCode.Space)) {
             this._rigidbody.AddForce(this.transform.TransformDirection(Vector3.up) * Time.fixedDeltaTime * _forceY);
         }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
+        if (Input.GetKey(KeyCode.LeftShift)) {
             this._rigidbody.AddForce(this.transform.TransformDirection(Vector3.down) * Time.fixedDeltaTime * _forceY);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             this._rigidbody.AddTorque(this.transform.up * _rotationYSpeed * -1, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             this._rigidbody.AddTorque(this.transform.up * _rotationYSpeed, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
+        if (Input.GetKey(KeyCode.UpArrow)) {
             this._rigidbody.AddTorque(this.transform.right * _rotationYSpeed, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
+        if (Input.GetKey(KeyCode.DownArrow)) {
             this._rigidbody.AddTorque(this.transform.right * _rotationYSpeed * -1, ForceMode.Force);
         }
 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            this._rigidbody.AddTorque(this.transform.forward * _rotationYSpeed, ForceMode.Force);
+        if (Input.GetKey(KeyCode.Q)) {
+            this._rigidbody.AddTorque(this.transform.forward * _rotationYSpeed * 0.2f, ForceMode.Force);
         }
-        if (Input.GetKey(KeyCode.E))
-        {
-            this._rigidbody.AddTorque(this.transform.forward * _rotationYSpeed * -1, ForceMode.Force);
+        if (Input.GetKey(KeyCode.E)) {
+            this._rigidbody.AddTorque(this.transform.forward * _rotationYSpeed * -1 * 0.2f, ForceMode.Force);
         }
         this._rigidbody.angularVelocity *= 1 - Time.fixedDeltaTime * 4;
     }
