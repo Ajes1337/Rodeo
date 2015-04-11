cf6fcdfe28bf220bd0cf7197b223db1d81a659e2
Anders Nørgaard <Ajes1337@gmail.com>
2015-04-11 21:22:02 +0200

    ændret chunkwidth til 16 og viewrange ned til 12

--- Rodeo/Assets/Scripts/Ajes/Constants.cs
+++ Rodeo/Assets/Scripts/Ajes/Constants.cs
@@ -4,9 +4,9 @@ public class Constants {
 
 
     public static int WorkerAmount = 6;
-    public static int ChunkWidth = 12;
+    public static int ChunkWidth = 16;
     public static int ChunkHeight = 256;
-    public static int ViewRadiusChunk = 16;
+    public static int ViewRadiusChunk = 12;
     public static int AntalMaxChunksPerPortion = 16;
 
 
